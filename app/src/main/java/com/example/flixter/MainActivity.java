889411c58e1d8620d2ecd15078a2c57d4e3723ed
Abdiaziz Mohamoud <moha0469@d.umn.edu>
2020-06-25 09:45:15 -0500

    able to successfully connect to the movies api

--- app/src/main/java/com/example/flixter/MainActivity.java
+++ app/src/main/java/com/example/flixter/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.flixter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixter.models.movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=926ae7c470ef55994ae722645ebf3d7e";
+    public static final String TAG = "MainActivity";
+
+    List<movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "OnSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results" + results.toString());
+                    movies = movie.fromJsonArray(results);
+                    Log.i(TAG, "Movies " + movies.size());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit json exception");
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "OnFailure");
+            }
+        });
     }
 }
